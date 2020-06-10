43f6eba320d83ba39919b9cf2c3c13a98941ada3
Alexander Veselov <collabsis.g@gmail.com>
2020-06-10 20:37:23 +0400

    Добавьте файлы проекта.

---
+++ QR_code_scan/MainForm.Designer.cs
@@ -0,0 +1,100 @@
+﻿namespace QR_code_scan
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Stream_pictureBox = new System.Windows.Forms.PictureBox();
+            this.QrDecode_textBox = new System.Windows.Forms.TextBox();
+            this.StartStop_button = new System.Windows.Forms.Button();
+            this.Device_listBox = new System.Windows.Forms.ListBox();
+            ((System.ComponentModel.ISupportInitialize)(this.Stream_pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Stream_pictureBox
+            // 
+            this.Stream_pictureBox.Location = new System.Drawing.Point(12, 12);
+            this.Stream_pictureBox.Name = "Stream_pictureBox";
+            this.Stream_pictureBox.Size = new System.Drawing.Size(400, 400);
+            this.Stream_pictureBox.TabIndex = 0;
+            this.Stream_pictureBox.TabStop = false;
+            // 
+            // QrDecode_textBox
+            // 
+            this.QrDecode_textBox.Location = new System.Drawing.Point(418, 165);
+            this.QrDecode_textBox.Multiline = true;
+            this.QrDecode_textBox.Name = "QrDecode_textBox";
+            this.QrDecode_textBox.ReadOnly = true;
+            this.QrDecode_textBox.Size = new System.Drawing.Size(249, 247);
+            this.QrDecode_textBox.TabIndex = 1;
+            // 
+            // StartStop_button
+            // 
+            this.StartStop_button.Location = new System.Drawing.Point(418, 136);
+            this.StartStop_button.Name = "StartStop_button";
+            this.StartStop_button.Size = new System.Drawing.Size(249, 23);
+            this.StartStop_button.TabIndex = 2;
+            this.StartStop_button.Text = "Start scan";
+            this.StartStop_button.UseVisualStyleBackColor = true;
+            this.StartStop_button.Click += new System.EventHandler(this.StartStop_button_Click);
+            // 
+            // Device_listBox
+            // 
+            this.Device_listBox.FormattingEnabled = true;
+            this.Device_listBox.Location = new System.Drawing.Point(418, 12);
+            this.Device_listBox.Name = "Device_listBox";
+            this.Device_listBox.Size = new System.Drawing.Size(249, 121);
+            this.Device_listBox.TabIndex = 3;
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(682, 429);
+            this.Controls.Add(this.Device_listBox);
+            this.Controls.Add(this.StartStop_button);
+            this.Controls.Add(this.QrDecode_textBox);
+            this.Controls.Add(this.Stream_pictureBox);
+            this.Name = "MainForm";
+            this.Text = "Radioactive Raccoon: QR scan";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.MainForm_FormClosing);
+            this.Load += new System.EventHandler(this.MainForm_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.Stream_pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox Stream_pictureBox;
+        private System.Windows.Forms.TextBox QrDecode_textBox;
+        private System.Windows.Forms.Button StartStop_button;
+        private System.Windows.Forms.ListBox Device_listBox;
+    }
+}
+
---
+++ QR_code_scan/MainForm.cs
@@ -0,0 +1,105 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Threading;
+using System.Windows.Forms;
+using AForge.Video;
+using AForge.Video.DirectShow;
+using ZXing;
+
+namespace QR_code_scan
+{
+    public partial class MainForm : Form
+    {
+        private FilterInfoCollection videoDevice;
+        private VideoCaptureDevice videoSource;
+        private BarcodeReader reader;
+
+        public bool StartClickState { get; set; }
+
+        public string TempQrDecode { get; set; }
+
+
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        private void MainForm_Load(object sender, EventArgs e)
+        {
+            // search available webCam
+            videoDevice = new FilterInfoCollection(FilterCategory.VideoInputDevice);
+            if (videoDevice.Count != 0)
+                foreach (FilterInfo device in videoDevice)
+                    Device_listBox.Items.Add(device.Name);
+            Device_listBox.SelectedIndex = 0;
+
+            // init QR code reader
+            reader = new BarcodeReader();
+            reader.Options.PossibleFormats = new List<BarcodeFormat>
+            {
+                BarcodeFormat.QR_CODE
+            };
+
+            StartClickState = false;
+            TempQrDecode = string.Empty;
+        }
+
+        private void StartStop_button_Click(object sender, EventArgs e)
+        {
+
+            if (!StartClickState)
+            {
+                // start camera stream
+                videoSource = new VideoCaptureDevice(videoDevice[Device_listBox.SelectedIndex].MonikerString);
+                videoSource.NewFrame += new NewFrameEventHandler(stream);
+                videoSource.Start();
+
+                StartStop_button.Text = "Stop scan";
+                StartClickState = true;
+            }
+            else 
+            {
+                if (videoSource != null)
+                {
+                    // stop camera stream
+                    videoSource.SignalToStop();
+                    videoSource.WaitForStop();
+
+                    StartClickState = false;
+                    StartStop_button.Text = "Start scan";
+                    Stream_pictureBox.Image = null;
+                    QrDecode_textBox.Text = null;
+                }
+            }
+        }
+
+        // Decode event
+        private void stream(object sender, NewFrameEventArgs eventArgs)
+        {
+            // NewFrameEventArgs -- event from AForge.Video class
+            Bitmap bitmap = (Bitmap)eventArgs.Frame.Clone();
+            Stream_pictureBox.Image = bitmap;
+
+            // send image from camera in ZXing class, and decode them
+            Result result = reader.Decode((Bitmap)eventArgs.Frame.Clone());
+            if (result != null && result.Text != TempQrDecode)
+            {
+                Invoke((MethodInvoker)delegate
+                      {
+                          QrDecode_textBox.AppendText("\r\n" + result.Text);
+                      });
+                TempQrDecode = result.Text;
+            }
+        }
+
+        private void MainForm_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            if(videoSource != null)
+            {
+                videoSource.SignalToStop();
+                videoSource.WaitForStop();
+            }
+        }
+    }
+}
